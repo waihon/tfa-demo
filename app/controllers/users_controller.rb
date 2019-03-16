40c49a5a9939a0e56b30a81df14f958b0d4c3272
Waihon Yew <yewwaihon@gmail.com>
2019-03-16 10:57:56 +0800

    Users destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "Account successfully deleted!"
+    redirect_to root_path
+  end
+
   private
 
   def user_params
