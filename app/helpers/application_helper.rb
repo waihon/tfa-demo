58f07f712dadeb5003e6162f149a6060e1d757d7
Waihon Yew <yewwaihon@gmail.com>
2019-03-16 12:44:17 +0800

    Enhance flash partial and messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  # https://gist.github.com/roberto/3344628
+  def bootstrap_class_for(flash_type)
+    case flash_type.to_s
+    when "success"
+      "alert-success" # Green
+    when "error"
+      "alert-danger" # Red
+    when "alert"
+      "alert-warning" # Yellow
+    when "notice"
+      "alert-info" # Blue
+    else
+      flash_type.to_s
+    end
+  end
 end
