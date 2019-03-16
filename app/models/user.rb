80b3f97f4d87a6a54f36dcd82ee4c6a3520e1c97
Waihon Yew <yewwaihon@gmail.com>
2019-03-16 23:58:18 +0800

    Add a profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@ class User < ApplicationRecord
     user = User.find_by(email: email)
     user && user.authenticate(password)
   end
+
+  def gravatar_id
+    Digest::MD5.hexdigest(email.downcase)
+  end
 end
