4fc3493b09e104f1748b2cf47c16b4488560921e
Waihon Yew <yewwaihon@gmail.com>
2019-03-17 16:07:14 +0800

    Add a feature spec for signing in

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+describe "Signing in" do
+  it "prompts for an email and password" do
+    visit root_url
+
+    click_link "Sign In"
+
+    expect(current_path).to eq(new_session_path)
+
+    expect(page).to have_field("Email")
+    expect(page).to have_field("Password")
+  end
+end
