80489fb00af39a75ca8ada3a6f51f7a4625d5775
Waihon Yew <yewwaihon@gmail.com>
2019-03-19 08:06:16 +0800

    Sign-in authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    fail
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = "Invalid email/password combination!"
+      render :new
+    end
   end
 end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "Signing in", js: true do
   it "prompts for an email and password" do
-    # Capybara somehow couldn't recognize any link with Bootstrap navbar-collapse
+    # Capybara somehow couldn't recognize any link within Bootstrap's navbar-collapse
     visit signin_path
 
     expect(current_path).to eq(signin_path)
@@ -26,5 +26,36 @@ describe "Signing in", js: true do
 
     message = page.find("#password").native.attribute("validationMessage")
     expect(message).to eq "Please fill out this field."
+  end
+
+  it "signs in the user if the email/password combination is valid" do
+    user = User.create!(user_attributes)
+
+    visit signin_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button "Sign In"
+
+    expect(current_path).to eq(user_path(user))
+
+    expect(page).to have_text("Welcome back, #{user.name}!")
+  end
+
+  it "does not sign in the user if the email/password combination is invalid" do
+    user = User.create!(user_attributes)
+
+    visit signin_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "invalid_#{user.password}"
+
+    click_button "Sign In"
+
+    # HTML5 required validation has been passed so :new view is rendered
+    expect(current_path).to eq(session_path)
+
+    expect(page).to have_text("Invalid email/password combination")
   end
 end
