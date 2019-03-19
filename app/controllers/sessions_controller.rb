a98f891d8c0f49f0e5d67c764f6fd876cb85860f
Waihon Yew <yewwaihon@gmail.com>
2019-03-19 23:20:07 +0800

    Add username as an alternative credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if user = User.authenticate(params[:email], params[:password])
+    if user = User.authenticate(params[:email_or_username], params[:password])
       session[:user_id] = user.id
       redirect_to user, notice: "Welcome back, #{user.name}!"
     else
-      flash.now[:alert] = "Invalid email/password combination!"
+      flash.now[:alert] = "Invalid email/username/password combination!"
       render :new
     end
   end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,24 +1,39 @@
 require "rails_helper"
 
-describe "Signing in", js: true do
-  it "prompts for an email and password" do
+describe "Signing in" do
+  it "prompts for an email/username and password" do
     # Capybara somehow couldn't recognize any link within Bootstrap's navbar-collapse
     visit signin_path
 
     expect(current_path).to eq(signin_path)
 
-    expect(page).to have_field("Email")
+    expect(page).to have_field("Email or Username")
     expect(page).to have_field("Password")
     expect(page).to have_button("Sign In")
     expect(page).to have_link("Sign up!")
   end
 
-  it "stays on the same page when email or password is not entered" do
+  it "stays on the same page when email/username is not entered", js: true do
     visit signin_path
 
     expect(current_path).to eq(signin_path)
 
-    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "secret"
+
+    click_button "Sign In"
+
+    expect(current_path).to eq(signin_path) # stays on the same page
+
+    message = page.find("#email_or_username").native.attribute("validationMessage")
+    expect(message).to eq "Please fill out this field."
+  end
+
+  it "stays on the same page when password is not entered", js: true do
+    visit signin_path
+
+    expect(current_path).to eq(signin_path)
+
+    fill_in "Email or Username", with: "user@example.com"
 
     click_button "Sign In"
 
@@ -33,7 +48,7 @@ describe "Signing in", js: true do
 
     visit signin_path
 
-    fill_in "Email", with: user.email
+    fill_in "Email or Username", with: user.email
     fill_in "Password", with: user.password
 
     click_button "Sign In"
@@ -43,12 +58,59 @@ describe "Signing in", js: true do
     expect(page).to have_text("Welcome back, #{user.name}!")
   end
 
-  it "does not sign in the user if the email/password combination is invalid" do
+  it "signs in the user if the username/password combination is valid" do
     user = User.create!(user_attributes)
 
     visit signin_path
 
-    fill_in "Email", with: user.email
+    fill_in "Email or Username", with: user.username
+    fill_in "Password", with: user.password
+
+    click_button "Sign In"
+
+    expect(current_path).to eq(user_path(user))
+
+    expect(page).to have_text("Welcome back, #{user.name}!")
+  end
+
+  it "does not sign in the user if the email is invalid" do
+    user = User.create!(user_attributes)
+
+    visit signin_path
+
+    fill_in "Email or Username", with: "invalid" + user.email
+    fill_in "Password", with: user.password
+
+    click_button "Sign In"
+
+    # HTML5 required validation has been passed so :new view is rendered
+    expect(current_path).to eq(session_path)
+
+    expect(page).to have_text("Invalid email/username/password combination!")
+  end
+
+  it "does not sign in the user if the username is invalid" do
+    user = User.create!(user_attributes)
+
+    visit signin_path
+
+    fill_in "Email or Username", with: "invalid" + user.username
+    fill_in "Password", with: user.password
+
+    click_button "Sign In"
+
+    # HTML5 required validation has been passed so :new view is rendered
+    expect(current_path).to eq(session_path)
+
+    expect(page).to have_text("Invalid email/username/password combination!")
+  end
+
+  it "does not sign in the user if the password is invalid" do
+    user = User.create!(user_attributes)
+
+    visit signin_path
+
+    fill_in "Email or Username", with: user.email
     fill_in "Password", with: "invalid_#{user.password}"
 
     click_button "Sign In"
@@ -56,6 +118,6 @@ describe "Signing in", js: true do
     # HTML5 required validation has been passed so :new view is rendered
     expect(current_path).to eq(session_path)
 
-    expect(page).to have_text("Invalid email/password combination")
+    expect(page).to have_text("Invalid email/username/password combination!")
   end
 end
