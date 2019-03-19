043a1d7cbad0e6bd52f0efca4a48e524879bff2b
Waihon Yew <yewwaihon@gmail.com>
2019-03-19 21:56:35 +0800

    Encapsulate authentication logic in User model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    if user = User.authenticate(params[:email], params[:password])
       session[:user_id] = user.id
       redirect_to user, notice: "Welcome back, #{user.name}!"
     else
