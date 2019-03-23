507eb20de88c85b7339012ba5e1f9751c9844f13
Waihon Yew <yewwaihon@gmail.com>
2019-03-23 22:48:25 +0800

    Improve performance of current_user using memoization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,6 @@ module ApplicationHelper
   end
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
