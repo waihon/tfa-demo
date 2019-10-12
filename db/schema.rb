3d49523cbe7878395fbba0aca77b40679fc0339e
Waihon Yew <yewwaihon@gmail.com>
2019-10-12 18:06:25 +0800

    Create Todo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_06_140901) do
+ActiveRecord::Schema.define(version: 2019_10_12_095257) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
